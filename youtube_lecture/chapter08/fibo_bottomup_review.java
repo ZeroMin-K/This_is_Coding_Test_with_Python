2ec32a19be71c544e496a9e4835e415477097d3d
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-10-20 20:34:20 +0900

    Create fibo_bottomup_review.java

---
+++ youtube_lecture/chapter08/fibo_bottomup_review.java
@@ -0,0 +1,23 @@
+package chapter08;
+
+public class fibo_bottomup_review {
+	
+	public static long[] d = new long[100];
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		d[1] = 1;
+		d[2] = 1;
+		int n = 50;
+		
+		for ( int i = 3; i <= n; i++) {
+			d[i] = d[i-1] + d[i-2];
+		}
+		
+		System.out.println(d[n]);
+
+	}
+
+}
