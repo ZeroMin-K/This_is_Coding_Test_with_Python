d84e70a29255630b9df67e8d952d136ba545bb62
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-11-04 21:33:09 +0900

    Create disjoint_sets_ex.java

---
+++ youtube_lecture/chapter10/disjoint_sets_ex.java
@@ -0,0 +1,65 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class disjoint_sets_ex {
+	
+	// 노드의 개수 (V)와 간선(Union연산)의 개수(E)
+	// 노드의 개수는 최대 100,000개라고 가정
+	public static int v, e;
+	public static int[] parent = new int[100001];	// 부모 테이블 초기화
+	
+	// 특정 원소가 속한 집합 찾기
+	public static int findParent(int x) {
+		// 루트노드가 아니라면 루트 노드를 찾을 때까지 재귀적으로 호출
+		if (x == parent[x]) 
+			return x;
+		return findParent(parent[x]);
+	}
+	
+	// 두 원소가 속한 집합을 합치기
+	public static void unionParent(int a, int b) {
+		a = findParent(a);
+		b = findParent(b);
+		if (a < b)
+			parent[b] = a;
+		else
+			parent[a] = b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		v = sc.nextInt();
+		e = sc.nextInt();
+		
+		// 부모 테이블 상에서 부모를 자기자신으로 초기화
+		for (int i = 1; i <= v; i++) {
+			parent[i] = 1;
+		}
+		
+		// Union 연산을 각각 수행
+		for(int i = 0; i < e; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			unionParent(a, b);
+		}
+		
+		// 각 원소가 속한 집합 출력하기
+		System.out.print("각 원소가 속한 집합: ");
+		for (int i =1; i <= v; i++) {
+			System.out.print(findParent(i) + " ");
+		}
+		System.out.println();
+		
+		// 부모 테이블 내용 출력하기
+		System.out.print("부모 테이블: ");
+		for (int i = 1; i <= v; i++) {
+			System.out.print(parent[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+}
