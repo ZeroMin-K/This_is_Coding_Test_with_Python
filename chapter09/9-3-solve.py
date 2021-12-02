13a448778bf6f8ea86f3eca7084736c68542ee74
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-12-02 21:29:42 +0900

    Create 9-3-solve.py

---
+++ chapter09/9-3-solve.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+INF = int(1e9)
+
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for node in graph[now]:
+            cost = dist + node[1]
+            if cost < distance[node[0]]:
+                distance[node[0]] = cost
+                heapq.heappush(q, (cost, node[0]))
+
+dijkstra(c)
+
+count = 0
+max_distance = 0
+for node in distance:
+    if node != INF and node != 0:
+        count += 1
+        max_distance = max(node, max_distance)
+
+print(count, max_distance)
