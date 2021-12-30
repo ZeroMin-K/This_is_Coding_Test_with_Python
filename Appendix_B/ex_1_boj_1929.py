f44294544532f7d774d1df1ae889c4a9098d1bc3
ZeroMin-K <77002670+ZeroMin-K@users.noreply.github.com>
2021-12-30 19:34:23 +0900

    Create  ex_1_boj_1929.py

---
+++ Appendix_B/ex_1_boj_1929.py
@@ -0,0 +1,16 @@
+import math 
+
+m, n = map(int, input().split())
+
+prime_list = [True] * (n+1)
+prime_list[1] = False
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    j = 2
+    while i * j <= n:
+        prime_list[i * j] = False
+        j += 1
+
+for k in range(m, n+1):
+    if prime_list[k]:
+        print(k)
