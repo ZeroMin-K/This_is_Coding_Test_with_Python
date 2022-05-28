d4e9ac74691bb6a31e73d8c6494d9e913c935e30
Zero_Min <ymkim9510@gmail.com>
2022-05-29 00:17:16 +0900

    This is Coding Test review for coding test 2

---
+++ review_for_coding_test/SFY8/count_sort.py
@@ -0,0 +1,10 @@
+array = []
+# 모든 범위를 포함하는 리스트 선언
+count = [0] * (max(array) +1)
+
+for i in range(len(array)):
+    count[array[i]] += 1        # 각 데이터에 해당하는 인덱스값 증가
+
+for i in range(len(count)):     # 리스트에 기록된 정렬 정보 확인
+    for j in range(count[i]):
+        print(i, end=' ')
