b55c8af7fef37cdeb06a33b472e6f2d8a9984656
ZeroMin-K <ymkim9510@gmail.com>
2022-07-08 00:19:32 +0900

    유형별 기출문제 풀이 chapter15 이진탐색문제 Q27 367p

---
+++ chapter15/15-27-solve.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left, bisect_right
+
+n, x = map(int, input().split())
+data = list(map(int, input().split()))
+
+right = bisect_right(data, x)
+left = bisect_left(data, x)
+
+count = right - left
+if count:
+    print(count)
+else:
+    print(-1)
