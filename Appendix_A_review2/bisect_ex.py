4e2949a67a89b133d15e8f02d0881c2b0f329fa7
ZeroMin-K <ymkim9510@gmail.com>
2022-10-06 23:50:28 +0900

    코딩 테스트 위한 파이썬 문법 - 449 ~ 463p

---
+++ Appendix_A_review2/bisect_ex.py
@@ -0,0 +1,21 @@
+from bisect import bisect_left, bisect_right
+
+a = [1, 2, 4, 4, 8]
+x = 4
+
+print(bisect_left(a, x))
+print(bisect_right(a, x))
+
+# 값이 [left_value, right_value]인 데이터 개수 반환하는 함수
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+
+    return right_index - left_index
+
+# 리스트 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+print(count_by_range(a, 4, 4))
+
+print(count_by_range(a, -1, 3))
